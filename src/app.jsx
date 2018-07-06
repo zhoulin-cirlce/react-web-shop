186447a57036e2e4418e6df110cc65edb4bd1885
zhoulin-cirlce <zlinjiejie@sina.com>
2018-07-06 14:31:47 +0800

    initial

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+ReactDom.render(
+    <div>
+        <i className="fa fa-address-book"></i>
+        <h1>Hello World!</h1>
+        
+    </div>,
+    document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const webpack =require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const ExtractTextPlugin =require('extract-text-webpack-plugin');
+module.exports={
+    entry:'./src/app.jsx',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        publicPath:'/dist/',
+        filename:"js/app.js"
+
+    },
+    module:{
+        rules:[
+            {
+                test:/\.jsx$/,
+                exclude:/(node_modules)/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:['env','react']
+                    }
+                }
+            },
+            {
+                test:/\.css$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:"css-loader"
+                })
+            },
+            {
+                test:/\.scss$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:["css-loader","sass-loader"]
+                })
+            },
+            {
+                test:/\.(png|jpg|gif)$/,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{
+                            limit:12192,
+                            name:'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test:/\.(eot|svg|ttf|woff|woff2|otf)$/,
+                use:[
+                    {
+                        loader:'url-loader',
+                        options:{
+                            limit:12192,
+                            name:'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        //处理html文件，指定html模版文件生成
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        }),
+        //css文件独立生成
+        new ExtractTextPlugin('css/[name].css'),
+        //提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'common',
+            filename:'js/base.js'
+        })
+    ],
+    devServer:{
+        port:3000
+    }
+
+}
